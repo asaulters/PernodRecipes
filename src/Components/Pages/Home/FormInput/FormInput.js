fcfd91b9b5797c800ad366e4e22b3153cfd1df56
Adam Saulters <adamsaulters@Adams-Air.attlocal.net>
2022-09-26 05:46:14 -0400

    added inputs to form.

--- src/Components/Pages/Home/FormInput/FormInput.js
+++ src/Components/Pages/Home/FormInput/FormInput.js
@@ -3,10 +3,34 @@ import React from 'react';
 import classes from './FormInput.module.css';
 
 const FormInput = () => {
+
+    const addLiquor =(e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+    }
+
+
   return (
     <div className={classes.formInputWrapper}>
         <form>
-            
+            <div className={classes.liquorsDiv}>
+                <label for='liquor'>Liquors</label>
+                <input type='text' name='liquor' className={classes.liquorInput}/>
+                <button
+                    onClick={addLiquor}
+                >Add Liquor</button>
+            </div>
+            <div className={classes.liqueursDiv}>
+                <label for='liqueur'>Liqueurs</label>
+                <input type='text' name='liqueur' className={classes.liqueurInput}/>
+                <button>Add Liqueur</button>
+            </div>
+            <div className={classes.liquorsDiv}>
+                <label for='mixers'>Mixers</label>
+                <input type='text' name='mixers' className={classes.mixersInput} />
+                <button>Add mixers</button>
+            </div>
+            <button>Submit</button>
         </form>
     </div>
   )
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import classes from './Home.module.css'
+import FormInput from './FormInput/FormInput'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={classes.homeWrapper}>
+      <FormInput />
+    </div>
   )
 }
 
